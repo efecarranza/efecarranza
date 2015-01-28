9e772d1ce3bcc6fa7e97b0abb5ad74e26f088d73
piscu Carranza <ferchiincarranza@gmail.com>
2015-01-28 15:13:00 -0500

    server.rb now takes path to all different parts of website and hides file extensions.

--- server.rb
+++ server.rb
@@ -1,13 +1,37 @@
 require 'sinatra'
 
+# Set main website to default to efecarranza.html file
+
 get '/' do
 	File.read(File.join('public', 'efecarranza.html'))
+end
+
+# Set path to hide file extensions on website
+
+get '/profile' do
+	File.read(File.join('public', 'profile.html'))	
 end
 
 get '/blog' do
 	File.read(File.join('public', 'blog.html'))	
 end
 
-# get '/sinatra' do
-# 	"Hello Sinatra."
-# end
+get '/photos' do
+	File.read(File.join('public', 'photos.html'))	
+end
+
+get '/videos' do
+	File.read(File.join('public', 'videos.html'))	
+end
+
+get '/map' do
+	File.read(File.join('public', 'map.html'))	
+end
+
+get '/games' do
+	File.read(File.join('public', 'games.html'))	
+end
+
+get '/contact' do
+	File.read(File.join('public', 'contact.html'))	
+end
