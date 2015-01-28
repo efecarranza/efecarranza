a18754838936039eeeba7db0a0518afd0c457e95
piscu Carranza <ferchiincarranza@gmail.com>
2015-01-28 15:03:02 -0500

    Updated server.rb to create website path. Test one before committing all.

--- server.rb
+++ server.rb
@@ -4,6 +4,10 @@ get '/' do
 	File.read(File.join('public', 'efecarranza.html'))
 end
 
+get '/blog' do
+	File.read(File.join('public', 'blog.html'))	
+end
+
 # get '/sinatra' do
 # 	"Hello Sinatra."
 # end
