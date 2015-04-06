08c13c51a6ce7ec454a63d8cb62b2fae10d689fa
piscu Carranza <fcarr004@fiu.edu>
2015-04-06 13:32:49 -0400

    new style change

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 # Set main website to default to efecarranza.html file
 
 get '/' do
-	File.read(File.join('public', 'efecarranza.html'))
+	File.read(File.join('public', 'index.html'))
 end
 
 # Set path to hide file extensions on website
